e3e9c6da014a3bcdd2dfbd222941d2060b0915b6
prakash <prakashmeghgurung123@gmail.com>
2023-07-31 23:49:24 +0530

    initial project setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+   <Button variant="destructive">Click me here</Button>
+  )
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+const LandingPage = () => {
+    return (
+        <div> Hey gurung welcome to our new world</div>
+    );
+}
+
+export default LandingPage;
