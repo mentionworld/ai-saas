e3fb35698774a9af232f7fdcbeb2cec8aa0a4460
prakash <prakashmeghgurung123@gmail.com>
2023-08-03 23:47:02 +0530

    update the code for side bar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard  Page After login</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
